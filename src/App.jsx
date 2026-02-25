27881a55d153ba87de48049821ef3850ebf01638
james65 <jamestonibor65@gmail.com>
2026-02-25 17:04:23 +0100

    sidebar config complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { Toaster } from "react-hot-toast";
 import Display from "./components/Display.jsx";
 import AuthWrapper from "./components/AuthWrapper.jsx";
+import SideBar from "./components/SideBar.jsx";
 
 const App = () => {
-  return(
- 
-     <>
-      <Toaster/>
-      <AuthWrapper>
-      <Display/>
-      </AuthWrapper>
-     </>
+  return (
 
-    
+    <>
+      <Toaster />
+      <AuthWrapper>
+        <div className="h-screen bg-black">
+          <div className="h-[90%] flex">
+            <SideBar/>
+            <Display />
+          </div>
+          {/* PLAYER COMPONENT */}
+        </div>
+
+      </AuthWrapper>
+    </>
+
+
   )
 }
 export default App;
