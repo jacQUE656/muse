d624feed9c12baf290b2ee7fd830fc9136a4f2a4
james65 <jamestonibor65@gmail.com>
2026-03-25 08:07:38 +0100

    display fixed

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -11,7 +11,6 @@ const Display = () => {
   const { albumsData } = useContext(PlayerContext);
   const location = useLocation();
   
-  // Logic helpers
   const isAlbum = location.pathname.includes("album");
   const albumId = location.pathname.split("/").pop();
   const displayRef = useRef(null);
@@ -29,22 +28,25 @@ const Display = () => {
   }, [isAlbum, bgColour]);
 
   return (
+    /* REMOVED: lg:w-[75%] and h-screen 
+       ADDED: h-full and removed fixed width to let App.jsx aside/main handle it */
     <div 
       ref={displayRef} 
-      className="w-full h-screen overflow-hidden flex flex-col bg-[#121212] lg:w-[75%]"
+      className="w-full h-full overflow-hidden flex flex-col bg-[#121212]"
     >
-      {/* Sticky NavBar: Stays at the top of the scrollable area */}
+      {/* Sticky NavBar */}
       <div className="sticky top-0 z-20 w-full bg-[#121212]/80 backdrop-blur-md border-b border-white/5">
         <div className="px-4 py-3 md:px-6">
           <NavBar />
         </div>
       </div>
 
-      {/* Main Content: Scrollable overflow */}
-      <main className="flex-1 overflow-y-auto overflow-x-hidden scrollbar-thin">
+      {/* Main Content: Scrollable */}
+      <main className="flex-1 overflow-y-auto overflow-x-hidden custom-scrollbar">
         <div className="p-4 md:p-6 lg:p-8">
           <Routes>
-            <Route path="/home" element={<DisplayHome />} />
+            {/* Note: Paths here are relative to the parent /home/* route */}
+            <Route path="/" element={<DisplayHome />} />
             <Route path="/albums/:id" element={<DisplayAlbum album={albumsData?.find(x => x.id == albumId)} />} />
             <Route path="/search" element={<Search />} />
             <Route path="/playlist/:id" element={<DisplayPlaylist />} />
