cbc432e91077cf3d06014ab2d7841487463a9ed8
james65 <jamestonibor65@gmail.com>
2026-03-25 08:25:01 +0100

    gg

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -45,9 +45,7 @@ const SideBar = () => {
                 <div className="px-1">
                     {!showSearchInput ? (
                         <div
-                            onClick={{navigate: () => navigate('/search')
-                                , handleSearchClick
-                            }}
+                            onClick={handleSearchClick}
                             className="flex items-center gap-4 px-2 py-3 hover:bg-white/5 rounded-lg transition-all cursor-pointer group"
                         >
                             <Search className="w-6 h-6 text-gray-400 group-hover:text-green-500" />
